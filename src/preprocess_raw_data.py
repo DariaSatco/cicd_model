99953ec1d15a586edee34b1d7c095985b5569f1e
DariaSatco <dasha.shatco@gmail.com>
2022-07-21 17:35:11 +0300

    created simple script to clean cencus data

---
+++ src/preprocess_raw_data.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import csv
+import yaml
+
+def remove_whitespc(csv_path: str) -> pd.DataFrame:
+    """
+    Reads csv file and strips cell values to remove 
+    redundant whitespaces
+    """
+    aList=[]
+    with open(csv_path, 'r') as f:
+        reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
+        for row in reader:
+            aList.append([x.strip() for x in row])
+            
+    clean_df = pd.DataFrame(aList[1:-1], columns=aList[0])
+    
+    return clean_df
+
+
+if __name__=="__main__":
+
+    with open('config.yaml') as f:
+        params = yaml.safe_load(f)
+    
+    raw_data_pth = params['raw_data']
+    prep_data_pth = params['preprocessed_data']
+
+    clean_data = remove_whitespc(raw_data_pth)
+    clean_data.to_csv(prep_data_pth, index=False)
