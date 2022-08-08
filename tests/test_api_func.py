bd9dccd41d5e8cefc19dad8c265b392540a9e996
DariaSatco <dasha.shatco@gmail.com>
2022-08-09 00:02:07 +0300

    added API tests for very negative and very positive samples

--- tests/test_api_func.py
+++ tests/test_api_func.py
@@ -21,32 +21,61 @@ def test_api_get_root():
     assert r.json() == {"message": "Welcome to Udacity project app!"}
 
 
-def test_api_post_predict_known_sample():
+def test_api_post_predict_negative_sample():
     """
     Test that model inference output has the right format
-    and returns predicted outcome label
+    and returns predicted outcome label  '<=50'
     """
-    sample = {'age': 29,
-              'workclass': 'Private',
-              'fnlgt': 236436,
-              'education': 'HS-grad',
-              'education-num': 9,
-              'marital-status': 'Never-married',
-              'occupation': 'Adm-clerical',
-              'relationship': 'Not-in-family',
-              'race': 'White',
-              'sex': 'Female',
-              'capital-gain': 0,
-              'capital-loss': 0,
-              'hours-per-week': 40,
-              'native-country': 'United-States'
-              }
+    sample = {'age': 17,
+            'workclass': 'Private',
+            'fnlgt': 41865,
+            'education': '10th',
+            'education-num': 6,
+            'marital-status': 'Never-married',
+            'occupation': 'Other-service',
+            'relationship': 'Own-child',
+            'race': 'White',
+            'sex': 'Female',
+            'capital-gain': 0,
+            'capital-loss': 0,
+            'hours-per-week': 25,
+            'native-country': 'United-States'
+            }
     body = simplejson.dumps(sample)
 
     r = client.post("/predict/", data=body)
 
     assert r.status_code == 200
-    assert r.json() in ['<=50K', '>50K']
+    assert r.json() == '<=50K'
+
+
+def test_api_post_predict_positive_sample():
+    """
+    Test that model inference output has the right format
+    and returns predicted outcome label  '>50'
+    """
+    sample = {'age': 47,
+            'workclass': 'Private',
+            'fnlgt': 102308,
+            'education': 'Masters',
+            'education-num': 14,
+            'marital-status': 'Married-civ-spouse',
+            'occupation': 'Exec-managerial',
+            'relationship': 'Husband',
+            'race': 'White',
+            'sex': 'Male',
+            'capital-gain': 99999,
+            'capital-loss': 0,
+            'hours-per-week': 50,
+            'native-country': 'United-States'
+            }
+    body = simplejson.dumps(sample)
+
+    r = client.post("/predict/", data=body)
+
+    assert r.status_code == 200
+    assert r.json() == '>50K'
+
 
 
 def test_api_post_predict_wrong_input():
